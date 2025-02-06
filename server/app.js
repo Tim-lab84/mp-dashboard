4b6427fd5c5541b24ad5382e6285b8be5bd1e922
Tim-lab84 <Timothycalvinwilliams@gmail.com>
2025-02-06 17:18:06 +0100

    build

--- server/app.js
+++ server/app.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const products = require("./assets/products-data.json");
 const cors = require("cors");
-
+const path = require("path");
 const app = express();
 const port = 5000;
 
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, "public", "client-build")));
+
 app.get("/api/products", (req, res) => {
   res.json([...products]);
+});
+
+app.use("*", (request, response) => {
+  response.sendFile(
+    path.join(__dirname, "public", "client-build", "index.html")
+  );
 });
 
 app.listen(port, () => {
